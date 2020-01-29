574fb33fa8c2cce6703704bbb1a43bf4cc153f81
makicamel <unright@gmail.com>
2020-01-29 23:42:56 +0900

    Refactor to do DRY

--- 4_x.rb
+++ 4_x.rb
@@ -1,25 +1,19 @@
 require "minruby"
 
 def evaluate(tree)
+  return tree[1] if tree[0] == "lit"
+
+  left = evaluate tree[1]
+  right = evaluate tree[2]
   case tree[0]
   when "+"
-    left = evaluate(tree[1])
-    right = evaluate(tree[2])
     left + right
   when "-"
-    left = evaluate(tree[1])
-    right = evaluate(tree[2])
     left - right
   when "*"
-    left = evaluate(tree[1])
-    right = evaluate(tree[2])
     left * right
   when "/"
-    left = evaluate(tree[1])
-    right = evaluate(tree[2])
     left / right
-  when "lit"
-    tree[1]
   end
 end
 
