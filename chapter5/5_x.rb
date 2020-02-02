bf5c1bdc940827b0e523f5ececd5c578e72348dd
makicamel <unright@gmail.com>
2020-02-02 12:02:33 +0900

    Introduce env

--- chapter5/5_x.rb
+++ chapter5/5_x.rb
@@ -1,43 +1,44 @@
 require "minruby"
 
-def evaluate(tree)
+def evaluate(tree, env)
   case tree[0]
   when "lit"
     tree[1]
   when "+"
-    evaluate(tree[1]) + evaluate(tree[2])
+    evaluate(tree[1], env) + evaluate(tree[2], env)
   when "-"
-    evaluate(tree[1]) - evaluate(tree[2])
+    evaluate(tree[1], env) - evaluate(tree[2], env)
   when "*"
-    evaluate(tree[1]) * evaluate(tree[2])
+    evaluate(tree[1], env) * evaluate(tree[2], env)
   when "/"
-    evaluate(tree[1]) / evaluate(tree[2])
+    evaluate(tree[1], env) / evaluate(tree[2], env)
   when "**"
-    evaluate(tree[1])**evaluate(tree[2])
+    evaluate(tree[1], env)**evaluate(tree[2], env)
   when "%"
-    evaluate(tree[1]) % evaluate(tree[2])
+    evaluate(tree[1], env) % evaluate(tree[2], env)
   when "<"
-    evaluate(tree[1]) < evaluate(tree[2])
+    evaluate(tree[1], env) < evaluate(tree[2], env)
   when "<="
-    evaluate(tree[1]) <= evaluate(tree[2])
+    evaluate(tree[1], env) <= evaluate(tree[2], env)
   when ">"
-    evaluate(tree[1]) > evaluate(tree[2])
+    evaluate(tree[1], env) > evaluate(tree[2], env)
   when ">="
-    evaluate(tree[1]) >= evaluate(tree[2])
+    evaluate(tree[1], env) >= evaluate(tree[2], env)
   when "=="
-    evaluate(tree[1]) == evaluate(tree[2])
+    evaluate(tree[1], env) == evaluate(tree[2], env)
   when "stmts"
     i = 1
     last = nil
     until tree[i].nil?
-      last = evaluate(tree[i])
+      last = evaluate(tree[i], env)
       i += 1
     end
     last
   when "func_call"
-    p evaluate(tree[2])
+    p evaluate(tree[2], env)
   end
 end
 
 tree = minruby_parse(minruby_load)
-evaluate(tree)
+env = {}
+evaluate(tree, env)
